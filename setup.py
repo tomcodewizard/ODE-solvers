03eecc6ea5c6a962fa19ac5fa5cd3c5d9d143716
tomcodewizard <147259317+tomcodewizard@users.noreply.github.com>
2024-02-03 12:59:12 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='tstools',
+      version='0.1',
+      description='A package to analyse timeseries',
+      url='myfancywebsite.com',
+      author='Spam Eggs',
+      packages=['tstools'],
+      install_requires = ["numpy", "matplotlib", "scipy"],
+      license='GPLv3')
