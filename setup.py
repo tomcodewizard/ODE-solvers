7a9448d1f15e2ee10c7155a2b7592aa0660ced09
tomcodewizard <147259317+tomcodewizard@users.noreply.github.com>
2024-02-03 15:54:40 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 from setuptools import setup
 
-setup(name='ODE-solvers',
-      version='0.1',
-      author='Tom Reed',
-      packages=['core'],
-      install_requires = ["numpy", "matplotlib", "scipy"],
-      license='GPLv3')
+if __name__ == "__main__":
+      setup()
