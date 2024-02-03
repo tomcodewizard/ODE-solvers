33f3fe6331b8b7eb5d7aab483be6e765208cdf74
tomcodewizard <147259317+tomcodewizard@users.noreply.github.com>
2024-02-03 17:43:25 +0000

    Update conf.py blanklines

--- solver/docs/conf.py
+++ solver/docs/conf.py
@@ -1,8 +1,6 @@
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -15,11 +13,8 @@ release = 'v1'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
-
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
